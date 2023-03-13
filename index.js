0403e1f72fb1e24029b3d7fec125f522a1f90872
Devin Jones <devin.jones21083@gmail.com>
2023-03-13 15:48:36 -0400

    Edited index.js

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(rideDist){
+  if (rideDist > 2500) {
+    return (`No can do.`)
+  } 
+    if (rideDist <= 400) {
+      return (`This one is on me!`);
+    } else {
+        if ((rideDist > 400) * (rideDist < 2000) === 1) {
+          return (`That will be twenty bucks.`);
+        } else {
+          if (rideDist > 2000) {
+            return (`I will gladly take your thirty bucks.`);
+          }
+        }
+      }
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(rideDestin){
+  let finalDec = `DOES NOT COMPUTE!!!`;
+  rideDestin === `NYC` ? (finalDec = `Ok, sounds good.`) : (finalDec = `No go.`);
+  return (finalDec);
+} 
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tipAmt){
+  switch (tipAmt) {
+    case `generous`:
+      return (`Thank you so much.`);
+    case `not as generous`:
+      return (`Thank you.`);
+    case `thanks for everything`:
+      return ('Bye.');
+  }
+} 
